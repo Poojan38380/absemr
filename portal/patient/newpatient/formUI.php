eaa34db3a2e48664500a04344aec4bd6eb07d107
DevTeam <ccopenemrdev@gmail.com>
2023-08-04 17:05:32 +0000

    signature updated

--- portal/patient/newpatient/formUI.php
+++ portal/patient/newpatient/formUI.php
@@ -20,12 +20,14 @@ function medicalHistoryTab($pid)
 function noticePracticeTab($pid)
 {
     $patient = sqlQuery("select concat(fname,' ', lname) as name from patient_data where pid = ?", [$pid]);
+    $onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
     require_once('./tabs/notice-practice-tab.php');
 }
 
 function releaseTab($pid)
 {
     $patient = sqlQuery("select fname, lname from patient_data where pid = ?", [$pid]);
+    $onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
     require_once('./tabs/release-tab.php');
 }
 
