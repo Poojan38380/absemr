8e21518d5987cdd7068cf931502ac30211a3fabc
juggernautsei <sherwingaddis@gmail.com>
2023-08-15 12:02:01 -0400

    Adding Google Meet?

--- interface/modules/custom_modules/oe-module-google-meet/index.php
+++ interface/modules/custom_modules/oe-module-google-meet/index.php
@@ -1,8 +1,8 @@
 <?php
 
-define('ROOT_PATH', __DIR__);
-define('CREDENTIAL_PATH', __DIR__ . '/credentials');
-define('ROOT_URL', 'http://localhost/meet');
+const ROOT_PATH = __DIR__;
+const CREDENTIAL_PATH = __DIR__ . '/credentials';
+const ROOT_URL = 'http://localhost/meet';
 
 require_once 'classes/Init.php';
 $meet = (new Init)->start();
