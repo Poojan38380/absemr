695f28e66bd997f6045cbd77b646e956bf611810
juggernautsei <sherwingaddis@gmail.com>
2023-08-30 13:05:09 -0400

    Adding video meeting button to Group Session if one refactoring code

--- interface/therapy_groups/therapy_groups_views/groupDetailsGeneralData.php
+++ interface/therapy_groups/therapy_groups_views/groupDetailsGeneralData.php
@@ -56,7 +56,7 @@ require_once dirname(__FILE__, 2) . '/../../interface/main/calendar/zoom_functio
                       <?php }?>
                          <button class="btn btn-warning" onclick="refreshme()"><?php echo xlt('Zoom Meeting Not Working Yet');?></button>
                         <?php
-                            echo enableVideoButton('dashboard', $groupData['group_id']); //This should display the button like in the patient dashboard
+                            echo enableVideoButton('dashboard', $pid); //This should display the button like in the patient dashboard
                         ?>
                     </div>
                 </div>
