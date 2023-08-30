66fa529199a5428a56388e4a76ab09d01ce4d67a
juggernautsei <sherwingaddis@gmail.com>
2023-08-30 14:47:06 -0400

    Adding video meeting button to Group Session if one refactoring code

--- interface/main/calendar/zoom_functions.php
+++ interface/main/calendar/zoom_functions.php
@@ -188,7 +188,7 @@ function enableVideoButton($callFrom = null, $pid = null)
         }
     } elseif ($callFrom == 'group') {
         if ($meetingUrl != '') {
-            return '<div style = "margin-bottom:10px"><a class = "btn btn-warning href = "' . $meetingUrl . '" target = "_blank">' . xlt('G Zoom') . '</a></div>';
+            return '<a class = "btn btn-warning href = "' . $meetingUrl . '" target = "_blank">' . xlt('G Zoom') . '</a>';
         }
     } else {
             $data = [];
