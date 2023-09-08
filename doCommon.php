23dc6742768ddbd1665775808a338a187e6c4964
Shimah-haque <fathimashima13@gmail.com>
2023-09-08 19:06:22 +0530

    update doCommon to add table

--- doCommon.php
+++ doCommon.php
@@ -7,51 +7,74 @@ print('</pre>');
 function init()
 {
     // for erver
-    $database = live();
+    // $database = live();
 
     // for local
-    // $database = localTest();
+    $database = localTest();
 
     $GLOBALS['userName'] = $database['username'];
     $GLOBALS['password'] = $database['password'];
     $GLOBALS['db'] = $database['database'];
 
     // Call the addColumn function
-    if (addColumn()) {
-        echo "Column added successfully";
+    // if (addColumn()) {
+    //     echo "Column added successfully";
+    // } else {
+    //     echo "Error adding column";
+    // }
+
+    // Call the createTable function
+    if (createTable()) {
+        echo "Table added successfully";
     } else {
-        echo "Error adding column";
+        echo "Error creating table";
     }
 }
 
 //for server
-function live()
-{
-    $database = [
-        'username' => 'absopenemr',
-        'password' => 'HdTp87iU82ExVrF!@#',
-        'database' => 'absopenemr',
-    ];
-
-    return $database;
-}
-
-// for localhost
-// function localTest()
+// function live()
 // {
 //     $database = [
-//         'username' => 'openemr1111',
-//         'password' => '5HMwK-9@Y2r8CRR',
-//         'database' => 'openemr1111',
+//         'username' => 'absopenemr',
+//         'password' => 'HdTp87iU82ExVrF!@#',
+//         'database' => 'absopenemr',
 //     ];
 
 //     return $database;
 // }
 
-function addColumn()
+// for localhost
+function localTest()
 {
-    $selectQuery = "ALTER TABLE patient_therapeutic_form ADD COLUMN fam_support_recovery TEXT";
-    return DBRun($GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['db'], $selectQuery);
+    $database = [
+        'username' => 'openemr1111',
+        'password' => '5HMwK-9@Y2r8CRR',
+        'database' => 'openemr1111',
+    ];
+
+    return $database;
+}
+
+// function addColumn()
+// {
+//     $selectQuery = "ALTER TABLE patient_therapeutic_form ADD COLUMN fam_support_recovery TEXT";
+//     return DBRun($GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['db'], $selectQuery);
+// }
+
+function createTable()
+{
+    $sql = "CREATE TABLE `patient_notice_form` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`full_document` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+	`date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	`groupname` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`user` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`authorized` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`activity` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`pid` BIGINT(20) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`) USING BTREE ) COLLATE='utf8_general_ci'";
+
+    return DBRun($GLOBALS['userName'], $GLOBALS['password'], $GLOBALS['db'], $sql);
 }
 
 function DBRun($username, $password, $dbname, $query)
