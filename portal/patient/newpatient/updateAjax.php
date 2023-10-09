74c76d7fa9b55f956245871f694a0ac148faf1f6
juggernautsei <sherwingaddis@gmail.com>
2023-10-09 02:17:21 -0400

    Rewriting save function

--- portal/patient/newpatient/updateAjax.php
+++ portal/patient/newpatient/updateAjax.php
@@ -26,7 +26,7 @@ if (isset($_POST['updateReferralTab'])) {
     file_put_contents('/var/www/html/traps/referralPost.txt', print_r($_POST, true));
     foreach ($_POST as $key => $value) {
         $sql = "UPDATE `patient_referral_form` SET ? = ? where id = ?";
-        if (!empty($_POST['id'])) {
+        if ($key = 'id') {
             continue;
         }
         sqlStatement($sql, [$key, $value, $_POST['id']]);
