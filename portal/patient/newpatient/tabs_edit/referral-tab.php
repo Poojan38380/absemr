ac3f405240ea3839af25f73de0a01f4e7af20127
juggernautsei <sherwingaddis@gmail.com>
2023-10-09 03:07:04 -0400

    Rewriting save function

--- portal/patient/newpatient/tabs_edit/referral-tab.php
+++ portal/patient/newpatient/tabs_edit/referral-tab.php
@@ -35,12 +35,12 @@
 <div class="row"><label>What type of living environment do you reside in?</label>
     <?= selectedDropdown('living_environment', 'living_environment', $referral['living_environment'] ?? '') ?>
 </div>
-<?php if ($referral['living_environment'] == 'Shelter') { ?>
+<?php //if ($referral['living_environment'] == 'Shelter') { ?>
 <div class="row">
     <h5>Shelter Name:</h5>
     <?php echo $referral['shelter_name'] ?>
 </div>
-<?php } ?>
+<?php //} ?>
 
 
 <div class="row" name="aptnumber" style="display:none">
