69ac1ca88d6ca527954d62eca7725b264f9c152d
juggernautsei <sherwingaddis@gmail.com>
2023-10-16 18:59:49 -0400

    refactoring form_edit and referral form

--- portal/patient/newpatient/form_edit.php
+++ portal/patient/newpatient/form_edit.php
@@ -21,7 +21,12 @@ $pid = $_SESSION['pid'];
 <body>
     <script>
         <?php require($GLOBALS['srcdir'] . '/js/xl/jquery-datetimepicker-2-5-4-alternate.js.php'); ?>
-        $LAB.script("<?= $GLOBALS['web_root']; ?>/portal/patient/scripts/app/onsitedocuments.js?v=<?= $GLOBALS['v_js_includes']; ?>").wait()
+        $LAB.script("<?php echo $GLOBALS['assets_static_relative']; ?>/underscore/underscore-min.js")
+            .script("<?php echo $GLOBALS['assets_static_relative']; ?>/moment/moment.js")
+            .script("<?php echo $GLOBALS['assets_static_relative']; ?>/backbone/backbone-min.js")
+            .script("<?php echo $GLOBALS['web_root']; ?>/portal/patient/scripts/app.js?v=<?php echo $GLOBALS['v_js_includes']; ?>")
+            .script("<?php echo $GLOBALS['web_root']; ?>/portal/patient/scripts/model.js?v=<?php echo $GLOBALS['v_js_includes']; ?>").wait()
+            .script("<?= $GLOBALS['web_root']; ?>/portal/patient/scripts/app/onsitedocuments.js?v=<?= $GLOBALS['v_js_includes']; ?>").wait()
             .script("<?= $GLOBALS['web_root']; ?>/portal/patient/scripts/app/onsiteportalactivities.js?v=<?= $GLOBALS['v_js_includes']; ?>")
             .wait(function() {
             page.init();
