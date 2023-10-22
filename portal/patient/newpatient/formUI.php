331cc932ccf385c1ef0075d2ab0f32cd9eebe410
juggernautsei <sherwingaddis@gmail.com>
2023-10-22 15:41:18 -0400

    Fixing notice

--- portal/patient/newpatient/formUI.php
+++ portal/patient/newpatient/formUI.php
@@ -64,11 +64,11 @@ function noticePracticeTabSaved($pid)
 }
 function noticePracticeTabEdit($pid)
 {
-    $patient = sqlQuery("select concat(fname,' ', lname) as name from patient_data where pid = ?", [$pid]);
-    $onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
+    //$patient = sqlQuery("select concat(fname,' ', lname) as name from patient_data where pid = ?", [$pid]);
+    //$onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
      //$referral = sqlQuery("select * from patient_notice_form where pid = ?", [$pid]);
      //var_dump($referral); //This shows the informed consent release form. but if I remove this line, neither of the forms show up.
-    require_once('./tabs_edit/notice-practice-tab.php');
+    //require_once('./tabs_edit/notice-practice-tab.php');
 }
 function releaseTab($pid)
 {
