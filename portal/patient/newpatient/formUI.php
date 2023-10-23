de9fe02a3fcef88e8d28e3867b191469e313be04
juggernautsei <sherwingaddis@gmail.com>
2023-10-22 21:00:27 -0400

    release form saved

--- portal/patient/newpatient/formUI.php
+++ portal/patient/newpatient/formUI.php
@@ -74,8 +74,9 @@ function releaseTabEdit($pid)
     $patient = sqlQuery("select fname, lname from patient_data where pid = ?", [$pid]);
     $onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
     $referral = sqlQuery("select * from patient_release_form where pid = ?", [$pid]);
-    file_put_contents('/var/www/html/traps/working_referral.log', print_r($referral, true));
-    require_once('./tabs_edit/release-tab.php');
+    //file_put_contents('/var/www/html/traps/working_referral.log', print_r($referral, true));
+    echo $referral['full_document'];
+    //require_once('./tabs_edit/release-tab.php');
 }
 
 function releaseTab($pid)
