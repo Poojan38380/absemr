de6d6d94bd40083544c7d4cb6f4ac283592d7c53
juggernautsei <sherwingaddis@gmail.com>
2023-10-27 08:31:28 -0400

    Show patients schedule

--- portal/patient/newpatient/form.php
+++ portal/patient/newpatient/form.php
@@ -289,7 +289,7 @@ $listOfAppointments = $patientAppointments->getAppointmentsForPatient($pid)
     </div>
 
 <?php } else { ?>
-    <div class="row">
+    <div class="col">
 
         <h3><?php echo xlt("Here are your scheduled appointments") ?></h3>
         <?php
