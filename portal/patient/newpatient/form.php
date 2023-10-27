2769690720e07d59b8dcca3efdfaecb83bcfbc7b
juggernautsei <sherwingaddis@gmail.com>
2023-10-27 08:09:26 -0400

    Show patients schedule

--- portal/patient/newpatient/form.php
+++ portal/patient/newpatient/form.php
@@ -296,7 +296,7 @@ $listOfAppointments = $patientAppointments->getAppointmentsForPatient($pid)
         //echo "<pre>";
         foreach ($listOfAppointments as $appt) {
             //var_dump($appt);
-            echo "<p>" . $appt['pc_eventDate'] . " " . $appt['pc_startTime'] . "</p>";
+            echo "<p>" . $appt['pc_eventDate'] . " " . $appt['pc_startTime'] . " " . $appt['pc_apptstatus']. "</p>";
         }
         //echo "</pre>";
         /*
