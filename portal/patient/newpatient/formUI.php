95d74a63bace828c4be6ec73358fb93377cdc23e
juggernautsei <sherwingaddis@gmail.com>
2023-12-27 10:10:56 -0500

    Adding signature to the notice

--- portal/patient/newpatient/formUI.php
+++ portal/patient/newpatient/formUI.php
@@ -81,7 +81,8 @@ function releaseTab($pid)
 {
     $patient = sqlQuery("select fname, lname from patient_data where pid = ?", [$pid]);
     $onsite_signature = sqlQuery("select type,user,sig_image as sign from onsite_signatures where pid = ?", [$pid]);
-    $names = sqlQuery("SELECT social_worker_name, parole_offc_name, doc_name, prob_offc_name, welfare_worker_name, lawyer_attroney_name FROM patient_referral_form WHERE pid = ?", [$pid]);
+    $names = sqlQuery("SELECT social_worker_name, parole_offc_name, doc_name, prob_offc_name, welfare_worker_name,
+       lawyer_attroney_name FROM patient_referral_form WHERE pid = ?", [$pid]);
     file_put_contents('/var/www/html/traps/names.log', print_r($names, true));
     require_once('./tabs/release-tab.php');
 }
